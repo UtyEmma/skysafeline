313ab792504e7273de35b0782f8e21175c104144
Utibe-Abasi Emmanuel <utyemma@gmail.com>
2024-05-15 11:47:48 +0100

    Handled waelclsd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -481,8 +481,8 @@
                 </div>
             </div>
         </div>
-        <div>
-            <span class="absolute left-0 top-0">
+        <div class="hidden md:block">
+            <span class="absolute  left-0 top-0">
                 <svg width="495" height="470" viewBox="0 0 495 470" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <circle cx="55" cy="442" r="138" stroke="white" stroke-opacity="0.04"
